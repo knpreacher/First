8fc8f0856d6eebee24b5e4c7e23b92eb90d637b7
knpreacher <knpreacher@gmail.com>
2017-02-02 12:03:19 +0300

    commit

--- app/src/main/java/com/example/knp/first/MainActivity.java
+++ app/src/main/java/com/example/knp/first/MainActivity.java
@@ -6,11 +6,64 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView iv;
+    String mCoords;
+    TextView tvCs;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int rrr=0;
+        float x = event.getX();
+        float y = event.getY();
+        int oldD;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+
+                /*
+                float mx1 = event.getX(0);
+                float my1 = event.getY(0);
+                float mx2 = event.getX(1);
+                float my2 = event.getY(1);
+
+                int diam = (int) Math.sqrt((mx1-mx2)*(mx1-mx2)+(my1-my2)*(my1-my2));
+                rrr = diam-iv.getLayoutParams().height;
+                */
+            case MotionEvent.ACTION_MOVE:
+                mCoords = "Coords: x = " + x + ", y = " + y;
+
+                iv.setRotationX(x);
+                iv.setRotationY(y);
+
+                //iv.setRotation(x);
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+
+                break;
+        }
+        if(event.getPointerCount()>1){
+            float mx1 = event.getX(0);
+            float my1 = event.getY(0);
+            float mx2 = event.getX(1);
+            float my2 = event.getY(1);
+
+            int diam = (int) Math.sqrt((mx1-mx2)*(mx1-mx2)+(my1-my2)*(my1-my2));
+
+            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(diam-rrr,diam-rrr);
+            lp.addRule(RelativeLayout.CENTER_VERTICAL);
+            lp.addRule(RelativeLayout.CENTER_HORIZONTAL);
+            iv.setLayoutParams(lp);
+        }
+        tvCs.setText(mCoords);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         iv = (ImageView)findViewById(R.id.imageView);
-
+        tvCs = (TextView)findViewById(R.id.tvCs);
         Button bleft = (Button)findViewById(R.id.bleft);
         Button bright = (Button)findViewById(R.id.bright);
 
@@ -78,7 +131,6 @@ public class MainActivity extends AppCompatActivity {
             };
         });
 
-        ActionBar ab = getSupportActionBar();
-        ab.hide();
+        getSupportActionBar().hide();
     }
 }
